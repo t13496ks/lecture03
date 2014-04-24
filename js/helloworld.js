ead829100b2b5367f29de5794f3b45d861798791
t13496ks <t10496ks@sfc.keio.ac.jp>
2014-04-24 12:34:33 +0900

    hello earth

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var helloearth = function(){
+	var message = document.getElementById("helloearth");
+	message.setAttribute("class", "");
+};
+
+var showHelloearthButton = document.getElementById("showHelloearth");
+showHelloearthButton.addEventListener("click", helloearth);
